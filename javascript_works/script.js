448b1718ed0ca92275326cab8bd5244b68e3bdcc
Anitta Joseph <anittarj430@gmail.com>
2021-09-09 11:57:38 +0530

    msg

---
+++ javascript_works/script.js
@@ -0,0 +1,36 @@
+//javascript programming
+
+// print()
+// printf()
+// cout<<
+// System.out.Println()
+
+// console.log 
+// console.log("hello world")
+
+// identifiers
+// lower_case,underscore-variable names
+// displayValues-function names
+// print_person()-snake notation -mostly used in python
+// printPerson()-camel case -used in js
+// first case-upper case = class name ex:PersonDetails()
+
+// variable - to represent a memory location
+// int num=2;
+// memory store data            num=10234|0010
+
+// var,let,const - keywords for variables
+// gloabally var is used
+
+var company="Luminar Technolabs";
+var loc_name="Kakkanad";
+console.log(company);
+console.log(loc_name);
+// age name "hey i am anitta 22 yera old"
+
+
+
+
+
+
+
